42b60ed935c0240a731e2f36419dbb41a45199ac
Kshitij <kshitijdumbre21@gmail.com>
2024-06-01 02:51:21 +0530

    chore: initialize base project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import HelloWorld from "@/components/hello-world";
+
+export default function RootPage() {
+    return (
+        <div>
+            <HelloWorld name="Kshitij" />
+        </div>
+    );
+}
